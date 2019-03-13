6d1a1006e520a3dbe0d5a0f6649b2489cbcf55a5
TomDev24 <mail.com67747@gmail.com>
2019-03-13 13:51:06 +0300

    Updates

---
+++ precache-manifest.45798ec78d7f0bc011b22ecfa44c485c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "135e1c7cabf36eae2e3a",
+    "url": "/skypype/static/js/runtime~main.88dcbc6f.js"
+  },
+  {
+    "revision": "e608f4c399ad0b87e51e",
+    "url": "/skypype/static/js/main.1fc7f3db.chunk.js"
+  },
+  {
+    "revision": "1dc3aa9538c469a8e040",
+    "url": "/skypype/static/js/2.801d3c51.chunk.js"
+  },
+  {
+    "revision": "e608f4c399ad0b87e51e",
+    "url": "/skypype/static/css/main.93d1bc37.chunk.css"
+  },
+  {
+    "revision": "9fb9f586be19498755eaf00950379596",
+    "url": "/skypype/index.html"
+  }
+];
